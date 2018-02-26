ff602820c9729dd7dd9efe650f40a2a878d8e0c2
Fahid Javid <fahidjavid@gmail.com>
2018-02-26 18:15:52 +0500

    Working with JSON and AJAX

---
+++ json-and-ajax/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>JSON and JavaScript</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<script type="text/javascript" src="javascript.js"></script>
+</body>
+</html>
