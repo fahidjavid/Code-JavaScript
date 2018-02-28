e9cc17fe1ab245d0a63281288984d6f916786e83
Fahid Javid <fahidjavid@gmail.com>
2018-02-28 17:21:47 +0500

    Completed JSON and AJAX

--- json-and-ajax/index.php
+++ json-and-ajax/index.php
@@ -5,6 +5,12 @@
 </head>
 <body>
 
+<header>
+	<h1>JSON and AJAX</h1>
+	<button id="btn">Fetch Info for 3 New Animals</button>
+</header>
+
+<div id="animal-info"></div>
 
 <script type="text/javascript" src="javascript.js"></script>
 </body>
