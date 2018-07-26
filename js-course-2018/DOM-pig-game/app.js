a91c2482956592bb8223b718bcb3ec9326689cee
Fahid Javid <fahidjavid@gmail.com>
2018-07-26 14:21:26 +0500

    Updating Scores and Changing the Active Player.

--- js-course-2018/DOM-pig-game/app.js
+++ js-course-2018/DOM-pig-game/app.js
@@ -27,8 +27,29 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     // 1. Random number
     var dice = Math.floor(Math.random() * 6) + 1;
 
-    // Display the result
+    // 2. Display the result
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next Player
+        activePlayer = activePlayer === 1 ? 0 : 1;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+    }
+
 });
