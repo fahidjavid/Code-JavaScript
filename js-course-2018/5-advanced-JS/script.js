a717de17a1c1c6e4e29e5341da956801331ef643
Fahid Javid <fahidjavid@gmail.com>
2018-07-31 20:19:43 +0500

    First Class Functions Passing Functions as Arguments.

--- js-course-2018/5-advanced-JS/script.js
+++ js-course-2018/5-advanced-JS/script.js
@@ -56,8 +56,8 @@ jane.calculateAge();
 var a = 23;
 var b = a;
 a = 46;
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
 // objects
 var obj1 = {
@@ -67,8 +67,8 @@ var obj1 = {
 
 var obj2 = obj1;
 obj1.age = 30;
-console.log(obj1);
-console.log(obj2);
+// console.log(obj1);
+// console.log(obj2);
 
 // functions
 var age = 26;
@@ -82,7 +82,49 @@ function change(a, b, c) {
     b.city = 'Lahore';
 }
 
-change(age, obj, arr);
+change(age, obj);
 
-console.log(age);
-console.log(obj.city);
+// console.log(age);
+// console.log(obj.city);
+
+
+////////////////////////////////
+// Lecture: Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arraycalc(arr, fn) {
+    var arrRes = [];
+    
+    for(var i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]));
+    }
+    
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return (2018-el);
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el){
+
+    if( el >= 18 && el <= 81 ) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+
+}
+
+var ages = arraycalc(years, calculateAge);
+var fullAges = arraycalc(ages, isFullAge);
+var heartRates = arraycalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(heartRates);
