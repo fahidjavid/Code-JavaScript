ca0bc7882c453264f919d9a30fc298e91eccaeb4
Fahid Javid <fahidjavid@gmail.com>
2018-09-19 02:54:10 +0500

    Restructured the project files.

--- index.php
+++ index.php
@@ -1,3 +1,2 @@
-<?php
-
-// silence
+<a href="/js-course-2018">JS Course 2018</a><br>
+<a href="/json-and-ajax">Json and Ajax</a><br>
--- js-course-2018/index.php
+++ js-course-2018/index.php
@@ -1,1 +1,3 @@
-<?php
+<a href="/js-course-2018/5-advanced-js/index.html">5-advanced-JS</a><br>
+<a href="/js-course-2018/4-DOM-pig-game/index.html">DOM-pig-game</a><br>
+<a href="/js-course-2018/3-how-it-works/index.html">how-it-works</a><br>
