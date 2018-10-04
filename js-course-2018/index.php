527baca6c3a02fca1f7ebc8994b412923f552e42
Fahid Javid <fahidjavid@gmail.com>
2018-10-04 19:14:00 +0500

    Updated the main sections file.

--- js-course-2018/index.php
+++ js-course-2018/index.php
@@ -1,3 +1,4 @@
-<a href="/js-course-2018/5-advanced-js/index.html">5-advanced-JS</a><br>
-<a href="/js-course-2018/4-DOM-pig-game/index.html">DOM-pig-game</a><br>
-<a href="/js-course-2018/3-how-it-works/index.html">how-it-works</a><br>
+<a href="/js-course-2018/3-how-it-works/index.html">How It Works</a><br>
+<a href="/js-course-2018/4-DOM-pig-game/index.html">DOM Pig Game</a><br>
+<a href="/js-course-2018/5-advanced-js/index.html">Advanced JS</a><br>
+<a href="/js-course-2018/6-budgety/index.html">Budgety</a><br>
